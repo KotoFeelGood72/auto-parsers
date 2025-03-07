d6c659d9b2bc9d12359e7cd8789b3442087cc211
KotoFeelGood72 <kotenkolove@yandex.ru>
2025-03-07 21:59:14 +0300

    ss

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -1,31 +1,15 @@
 const { chromium } = require('playwright');
 
-let browser;
-let context;
-
 async function startBrowser() {
-    if (!browser) {
-        browser = await chromium.launch({
-            headless: true,
-            args: [
-                "--no-sandbox",
-                "--disable-setuid-sandbox",
-                "--disable-gpu",
-                "--disable-dev-shm-usage"
-            ]
-        });
-        context = await browser.newContext();
-    }
-    return context;
+    return await chromium.launch({
+        headless: true,
+        args: [
+            "--no-sandbox",
+            "--disable-setuid-sandbox",
+            "--disable-gpu",
+            "--disable-dev-shm-usage"
+        ]
+    });
 }
 
-async function closeBrowser() {
-    if (browser) {
-        await browser.close();
-        browser = null;
-        context = null;
-        console.log("✅ Браузер закрыт.");
-    }
-}
-
-module.exports = { startBrowser, closeBrowser };
+module.exports = { startBrowser };
